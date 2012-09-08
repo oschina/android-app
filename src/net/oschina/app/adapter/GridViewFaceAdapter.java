d0e32c2e293c5a99286011a52df3b33a328e351a
weimin.yanwm <weimin.yanwm@aliyun-inc.com>
2012-09-08 13:59:08 +0800

    fix缓存的评论头像显示不正常

--- src/net/oschina/app/adapter/GridViewFaceAdapter.java
+++ src/net/oschina/app/adapter/GridViewFaceAdapter.java
@@ -1,97 +1,132 @@
 package net.oschina.app.adapter;
 
-import net.oschina.app.R;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+import net.oschina.app.R;
+import net.oschina.app.common.StringUtils;
 import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.text.Spannable;
+import android.text.SpannableStringBuilder;
+import android.text.style.ImageSpan;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.GridView;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 /**
  * 用户表情Adapter类
+ * 
  * @author liux (http://my.oschina.net/liux)
  * @version 1.0
  * @created 2012-8-9
  */
-public class GridViewFaceAdapter extends BaseAdapter
-{
+public class GridViewFaceAdapter extends BaseAdapter {
 	// 定义Context
-	private Context	mContext;
+	private Context mContext;
 	// 定义整型数组 即图片源
-	private int[] mImageIds;
+	private final static int[] mImageIds = new int[] { R.drawable.f001,
+			R.drawable.f002, R.drawable.f003, R.drawable.f004, R.drawable.f005,
+			R.drawable.f006, R.drawable.f007, R.drawable.f008, R.drawable.f009,
+			R.drawable.f010, R.drawable.f011, R.drawable.f012, R.drawable.f013,
+			R.drawable.f014, R.drawable.f015, R.drawable.f016, R.drawable.f017,
+			R.drawable.f018, R.drawable.f019, R.drawable.f020, R.drawable.f021,
+			R.drawable.f022, R.drawable.f023, R.drawable.f024, R.drawable.f025,
+			R.drawable.f026, R.drawable.f027, R.drawable.f028, R.drawable.f029,
+			R.drawable.f030, R.drawable.f031, R.drawable.f032, R.drawable.f033,
+			R.drawable.f034, R.drawable.f035, R.drawable.f036, R.drawable.f037,
+			R.drawable.f038, R.drawable.f039, R.drawable.f040, R.drawable.f041,
+			R.drawable.f042, R.drawable.f043, R.drawable.f044, R.drawable.f045,
+			R.drawable.f046, R.drawable.f047, R.drawable.f048, R.drawable.f049,
+			R.drawable.f050, R.drawable.f051, R.drawable.f052, R.drawable.f053,
+			R.drawable.f054, R.drawable.f055, R.drawable.f056, R.drawable.f057,
+			R.drawable.f058, R.drawable.f059, R.drawable.f060, R.drawable.f061,
+			R.drawable.f062, R.drawable.f063, R.drawable.f064, R.drawable.f065,
+			R.drawable.f067, R.drawable.f068, R.drawable.f069, R.drawable.f070,
+			R.drawable.f071, R.drawable.f072, R.drawable.f073, R.drawable.f074,
+			R.drawable.f075, R.drawable.f076, R.drawable.f077, R.drawable.f078,
+			R.drawable.f079, R.drawable.f080, R.drawable.f081, R.drawable.f082,
+			R.drawable.f083, R.drawable.f084, R.drawable.f085, R.drawable.f086,
+			R.drawable.f087, R.drawable.f088, R.drawable.f089, R.drawable.f090,
+			R.drawable.f091, R.drawable.f092, R.drawable.f093, R.drawable.f094,
+			R.drawable.f095, R.drawable.f096, R.drawable.f097, R.drawable.f098,
+			R.drawable.f099, R.drawable.f100, R.drawable.f101, R.drawable.f103,
+			R.drawable.f104, R.drawable.f105 };
 
-	public GridViewFaceAdapter(Context c)
-	{
+	public GridViewFaceAdapter(Context c) {
 		mContext = c;
-		mImageIds = new int[]{
-			R.drawable.f001,R.drawable.f002,R.drawable.f003,R.drawable.f004,R.drawable.f005,R.drawable.f006,
-			R.drawable.f007,R.drawable.f008,R.drawable.f009,R.drawable.f010,R.drawable.f011,R.drawable.f012,
-			R.drawable.f013,R.drawable.f014,R.drawable.f015,R.drawable.f016,R.drawable.f017,R.drawable.f018,
-			R.drawable.f019,R.drawable.f020,R.drawable.f021,R.drawable.f022,R.drawable.f023,R.drawable.f024,
-			R.drawable.f025,R.drawable.f026,R.drawable.f027,R.drawable.f028,R.drawable.f029,R.drawable.f030,
-			R.drawable.f031,R.drawable.f032,R.drawable.f033,R.drawable.f034,R.drawable.f035,R.drawable.f036,
-			R.drawable.f037,R.drawable.f038,R.drawable.f039,R.drawable.f040,R.drawable.f041,R.drawable.f042,
-			R.drawable.f043,R.drawable.f044,R.drawable.f045,R.drawable.f046,R.drawable.f047,R.drawable.f048,
-			R.drawable.f049,R.drawable.f050,R.drawable.f051,R.drawable.f052,R.drawable.f053,R.drawable.f054,
-			R.drawable.f055,R.drawable.f056,R.drawable.f057,R.drawable.f058,R.drawable.f059,R.drawable.f060,
-			R.drawable.f061,R.drawable.f062,R.drawable.f063,R.drawable.f064,R.drawable.f065,R.drawable.f067,
-			R.drawable.f068,R.drawable.f069,R.drawable.f070,R.drawable.f071,R.drawable.f072,R.drawable.f073,
-			R.drawable.f074,R.drawable.f075,R.drawable.f076,R.drawable.f077,R.drawable.f078,R.drawable.f079,
-			R.drawable.f080,R.drawable.f081,R.drawable.f082,R.drawable.f083,R.drawable.f084,R.drawable.f085,
-			R.drawable.f086,R.drawable.f087,R.drawable.f088,R.drawable.f089,R.drawable.f090,R.drawable.f091,
-			R.drawable.f092,R.drawable.f093,R.drawable.f094,R.drawable.f095,R.drawable.f096,R.drawable.f097,
-			R.drawable.f098,R.drawable.f099,R.drawable.f100,R.drawable.f101,R.drawable.f103,R.drawable.f104,
-			R.drawable.f105
-		};
+	}
+
+	public static int[] getImageIds() {
+		return mImageIds;
 	}
 
 	// 获取图片的个数
-	public int getCount()
-	{
+	public int getCount() {
 		return mImageIds.length;
 	}
 
 	// 获取图片在库中的位置
-	public Object getItem(int position)
-	{
+	public Object getItem(int position) {
 		return position;
 	}
 
-
 	// 获取图片ID
-	public long getItemId(int position)
-	{
+	public long getItemId(int position) {
 		return mImageIds[position];
 	}
 
-
-	public View getView(int position, View convertView, ViewGroup parent)
-	{
+	public View getView(int position, View convertView, ViewGroup parent) {
 		ImageView imageView;
-		if (convertView == null)
-		{
+		if (convertView == null) {
 			imageView = new ImageView(mContext);
 			// 设置图片n×n显示
 			imageView.setLayoutParams(new GridView.LayoutParams(85, 85));
 			// 设置显示比例类型
 			imageView.setScaleType(ImageView.ScaleType.CENTER);
-		}
-		else
-		{
+		} else {
 			imageView = (ImageView) convertView;
 		}
-		
+
 		imageView.setImageResource(mImageIds[position]);
-		if(position < 65)
-			imageView.setTag("["+position+"]");
-		else if(position < 100)
-			imageView.setTag("["+(position+1)+"]");
+		if (position < 65)
+			imageView.setTag("[" + position + "]");
+		else if (position < 100)
+			imageView.setTag("[" + (position + 1) + "]");
 		else
-			imageView.setTag("["+(position+2)+"]");
-		
+			imageView.setTag("[" + (position + 2) + "]");
+
 		return imageView;
 	}
 
+	private static Pattern pattern = Pattern.compile("\\[{1}([0-9]\\d*)\\]{1}");
+
+	/**
+	 * 将[12]之类的字符串替换为表情
+	 * 
+	 * @param context
+	 * @param tx
+	 * @param content
+	 */
+	public static void setText(Context context, TextView tx, String content) {
+		SpannableStringBuilder builder = new SpannableStringBuilder(content);
+		Matcher matcher = pattern.matcher(content);
+		while (matcher.find()) {
+			//使用正则表达式找出其中的数字
+			int position = StringUtils.toInt(matcher.group(1));
+			int resId = 0;
+			try {
+				resId = GridViewFaceAdapter.getImageIds()[position];
+				ImageSpan span = new ImageSpan(context,resId);
+				builder.setSpan(span, matcher.start(), matcher.end(),
+						Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+			} catch (ArrayIndexOutOfBoundsException e) {
+			}
+		}
+		tx.setText(builder);
+	}
 }
