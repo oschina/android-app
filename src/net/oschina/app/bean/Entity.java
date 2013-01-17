6a5bf77e7450151a3765fc7569c16d3d835df0f8
开源中国 <oschina.net@gmail.com>
2013-01-17 16:50:56 +0800

    Update src/net/oschina/app/bean/Entity.java
    删除无用代码

--- src/net/oschina/app/bean/Entity.java
+++ src/net/oschina/app/bean/Entity.java
@@ -1,7 +1,5 @@
 package net.oschina.app.bean;
 
-import java.text.SimpleDateFormat;
-
 /**
  * 实体类
  * @author liux (http://my.oschina.net/liux)
@@ -10,9 +8,6 @@ import java.text.SimpleDateFormat;
  */
 public abstract class Entity extends Base {
 
-	public final static SimpleDateFormat SDF_IN = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-	public final static SimpleDateFormat SDF_OUT = new SimpleDateFormat("yyyy-MM-dd HH:mm");
-	
 	protected int id;
 
 	public int getId() {
